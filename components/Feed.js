81e43d188ea23be6ee0804ff6f5eeec1541c7b08
calibosocarlo1201 <calibosocarlo1201@gmail.com>
2024-02-25 20:26:18 +0800

    make the feed section responsive

--- components/Feed.js
+++ components/Feed.js
@@ -3,13 +3,17 @@ import Stories from './Stories'
 
 const Feed = () => {
   return (
-    <main>
-      <section>
+    <main class="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
+      <section className='md:col-span-2'>
         {/* stories */}
         <Stories />
 
         {/* post */}
         <Posts />
+      </section>
+
+      <section className='hidden md:inline-gird md-col-span-1'>
+
       </section>
     </main>
   )
